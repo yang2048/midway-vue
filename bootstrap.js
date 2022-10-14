4459e7135f9f53024ce50ea5a9a465c16983355c
Y.Y <yang2048@qq.com>
2022-10-14 11:50:19 +0800

    666

--- bootstrap.js
+++ bootstrap.js
@@ -1,2 +1,14 @@
+'use strict';
+
+var AV = require('leanengine');
+
+AV.init({
+    appId: process.env.LEANCLOUD_APP_ID,
+    appKey: process.env.LEANCLOUD_APP_KEY,
+    masterKey: process.env.LEANCLOUD_APP_MASTER_KEY
+});
+// 如果不希望使用 masterKey 权限，可以删除
+AV.Cloud.useMasterKey();
+
 const { Bootstrap } = require('@midwayjs/bootstrap');
 Bootstrap.run();
