67cce19b7bda1224d9321976fe94c86c20397061
Y.Y <yang2048@qq.com>
2022-10-14 11:25:42 +0800

    555

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
-// server.js
-
 'use strict';
+
+var AV = require('leanengine');
+
+AV.init({
+    appId: process.env.LEANCLOUD_APP_ID,
+    appKey: process.env.LEANCLOUD_APP_KEY,
+    masterKey: process.env.LEANCLOUD_APP_MASTER_KEY
+});
+// 如果不希望使用 masterKey 权限，可以删除
+AV.Cloud.useMasterKey();
 
 const cfork = require('cfork');
 const util = require('util');
--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -4,6 +4,6 @@ export default {
   // use for cookie sign key, should change to your own and keep security
   keys: '1657263506362_2014',
   koa: {
-    port: 8002,
+    port: process.env.LEANCLOUD_APP_PORT || process.env.PORT || 7001
   },
 } as MidwayConfig;
